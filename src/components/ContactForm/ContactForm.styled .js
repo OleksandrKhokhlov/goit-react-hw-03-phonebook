bc26f275d253230f2d55daee7a1cf020c584b126
OleksandrKhokhlov <hohlovsanya@gmail.com>
2023-08-15 16:50:29 +0300

    settings project

---
+++ src/components/ContactForm/ContactForm.styled .js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const EntryField = styled.label`
+  width: 50%;
+  margin: 10px auto;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-content:space-around;
+  text-align: left;
+`;
